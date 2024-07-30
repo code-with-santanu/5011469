38b8f189ec48e014c30c05856ef684964d336327
code-with-santanu <santanupal003@gmail.com>
2024-07-31 00:58:47 +0530

    setup done

---
+++ week 1/Algorthms_Data Structures/Exercise 5 Task Management System/TaskManagementSystem/src/main/java/org/example/Task.java
@@ -0,0 +1,46 @@
+package org.example;
+
+public class Task {
+    private int taskId;
+    private String taskName;
+    private boolean status;
+
+    public Task(int taskId, String taskName, boolean status) {
+        this.taskId = taskId;
+        this.taskName = taskName;
+        this.status = status;
+    }
+
+    public int getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(int taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "taskId=" + taskId +
+                ", taskName='" + taskName + '\'' +
+                ", status=" + status +
+                '}';
+    }
+}
---
+++ week 1/Algorthms_Data Structures/Exercise 6 Library Management System/LibraryManagementSystem/src/main/java/org/example/Book.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Book {
+
+    private int bookId;
+
+    private String title;
+    private String author;
+}
