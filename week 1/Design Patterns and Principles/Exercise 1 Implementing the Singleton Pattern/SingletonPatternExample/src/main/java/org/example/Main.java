da551c987510ba9d3cafc9cbc7ddf317b8ea7564
code-with-santanu <santanupal003@gmail.com>
2024-08-02 00:04:45 +0530

    updated

--- week 1/Design Patterns and Principles/Exercise 1 Implementing the Singleton Pattern/SingletonPatternExample/src/main/java/org/example/Main.java
+++ week 1/Design Patterns and Principles/Exercise 1 Implementing the Singleton Pattern/SingletonPatternExample/src/main/java/org/example/Main.java
@@ -2,6 +2,18 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Get the only object available
+        Logger logger1 = Logger.getInstance();
+        System.out.println("The first instance: "+ logger1);
+        Logger logger2 = Logger.getInstance();
+        System.out.println("The second instance: "+ logger2);
+
+
+        // Verify that both logger1 and logger2 point to the same instance
+        if (logger1 == logger2) {
+            System.out.println("Both logger1 and logger2 are the same instance.");
+        } else {
+            System.out.println("logger1 and logger2 are different instances.");
+        }
     }
 }
